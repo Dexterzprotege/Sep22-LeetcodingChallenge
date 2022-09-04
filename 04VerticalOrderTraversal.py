6eec669f8e2a317fd11159e5b77937cde2f3ee62
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2022-09-04 22:45:12 +0530

    Create 04VerticalOrderTraversal.py

---
+++ 04VerticalOrderTraversal.py
@@ -0,0 +1,27 @@
+# Question: https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+
+# Solution:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        g = collections.defaultdict(list) 
+        queue = [(root,0)]
+        while queue:
+            new = []
+            d = collections.defaultdict(list)
+            for node, s in queue:
+                d[s].append(node.val) 
+                if node.left:  new += (node.left, s-1), 
+                if node.right: new += (node.right,s+1),  
+            for i in d: g[i].extend(sorted(d[i]))
+            queue = new
+        return [g[i] for i in sorted(g)]
+      
+# Verdict:
+# Runtime: 38 ms, faster than 88.69% of Python3 online submissions for Vertical Order Traversal of a Binary Tree.
+# Memory Usage: 14.2 MB, less than 28.65% of Python3 online submissions for Vertical Order Traversal of a Binary Tree.
